25c662e4d604d9a4aa013ed4df34ea54f3ffa149
opocaj92 <jacopo.castellini@gmail.com>
2016-01-25 12:17:59 +0100

    Tutto + README

---
+++ Conad Simulator.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+import simpy
+from random import randint, gauss, expovariate
+from copy import deepcopy
+from matplotlib import pyplot as plt
+import numpy as np
+
+class ConadSimulator:
+    def __init__(self, n_registers, duration, stable_at = 0):
+        self.env = simpy.Environment()
+        self.n_registers = n_registers
+        self.duration = duration
+        self.stable_at = stable_at
+        self.registers = list()
+        for i in xrange(n_registers):
+            self.registers.append(simpy.Resource(self.env, capacity = 1))
+        self.c_queue = 0
+        self.c_registers = 0
+        self.c_out = 0
+        self.ti = list()
+        self.ts = list()
+        self.tw = list()
+    def start(self):
+        self.env.process(self.entrance())
+        self.env.run(until = self.duration)
+    def customer(self):
+        arrival = self.env.now
+        j = randint(0, self.n_registers - 1)
+        with self.registers[j].request() as c:
+            yield c
+            if self.env.now > self.stable_at:
+                self.tw.append(self.env.now - arrival)
+            c_ts = -1
+            while c_ts < 0:
+                c_ts = gauss(94, 44)
+            if self.env.now > self.stable_at:
+                self.c_queue -= 1
+                self.c_registers += 1
+            yield self.env.timeout(c_ts)
+            if self.env.now > self.stable_at:
+                self.ts.append(c_ts)
+                self.c_registers -= 1
+                self.c_out += 1
+    def entrance(self):
+        i = 1
+        while True:
+            self.env.process(self.customer())
+            if self.env.now > self.stable_at:
+                self.c_queue += 1
+            i += 1
+            c_ti = expovariate(1.0 / 25)
+            if self.env.now > self.stable_at:
+                self.ti.append(c_ti)
+            yield self.env.timeout(c_ti)
+    def statistics(self):
+        print
+        print
+        print "####################################"
+        print "###         Statistics           ###"
+        print "####################################"
+        print
+        total = self.c_queue + self.c_registers + self.c_out
+        print "Total customers number: ", total
+        print "Enqueued customers number: ", self.c_queue
+        print "Customer at the cash registers: ", self.c_registers
+        print "Completed customers number: ", self.c_out
+        ti = np.mean(self.ti)
+        tw = np.mean(self.tw)
+        ts = np.mean(self.ts)
+        lambd = 1 / (self.n_registers * ti)
+        mu = 1 / ts
+        rho = lambd / mu
+        print "Mean arrival time: ", ti
+        print "Mean response time: ", tw + ts
+        print "Mean service time: ", ts
+        print "Mean waiting time: ", tw
+        print "Mean utilization: ", (float(sum(self.ts)) / (self.n_registers * self.duration))
+        print "Mean throughput: ", (float(self.c_out / sum(self.ts)))
+        print "Lambda: ", lambd
+        print "Mu: ", mu
+        print "Rho: ", rho,
+        if rho < 1:
+            print " -> Stable"
+        else:
+            print " -> Unstable"
+
+def stabilize(n_registers, duration, p):
+    results = [list() for i in xrange(p)]
+    total_mean = 0
+    instable = True
+    iteration = 0
+    cont = 5
+    total = list()
+    while instable:
+        iteration += 1
+        for i in xrange(p):
+            sim = ConadSimulator(n_registers, iteration * duration)
+            sim.start()
+            results[i] = deepcopy(sim.tw)
+        old_mean = total_mean
+        means = [np.mean(results[i]) for i in xrange(p)]
+        total_mean = np.mean(means)
+        total.append(total_mean)
+        if abs(old_mean - total_mean) < 0.02 * total_mean:
+            cont -= 1
+            if cont == 0:
+                instable = False
+        else:
+            cont = 5
+    print "Stabilized at iteration %d" % iteration
+    print "Mean: %lf" % total_mean
+    p10 = np.percentile(medie, 10)
+    p90 = np.percentile(medie, 90)
+    print "10° percentile: %lf" % p10
+    print "90° percentile: %lf" % p90
+    plt.plot([i for i in xrange(len(totale))], totale, "rx")
+    plt.hold(True)
+    plt.axhline(y = media_totale, color = "black", linestyle = "--")
+    plt.ylabel("Mean E(Tw)")
+    plt.xlabel("Iteration")
+    plt.legend(["Mean of means"], loc = 4)
+    plt.show()
+    return [iteration, total_mean, p10, p90]
+
+def repeated_measures(n_registers, duration, values, p):
+    outside = 0
+    inside = 0
+    results= list()
+    for i in xrange(p):
+        j = randint(values[0] / 2, values[0])
+        simulator = ConadSimulator(n_registers, (values[0] + j) * duration, values[0] * duration)
+        simulator.start()
+        total_mean = np.mean(simulator.tw)
+        results.append(x)
+        if total_mean >= values[2] and total_mean <= values[3]:
+            inside += 1
+        else:
+            outside += 1
+    print "Experiments between P10 and P90: %d" % inside
+    print "Experiments out of range: %d" % outside
+    plt.plot([i for i in xrange(len(results))], results, "rx")
+    plt.hold(True)
+    plt.axhline(y = values[1], color = "black", linestyle = "--")
+    plt.axhline(y = values[2], color = "black", linestyle = "--")
+    plt.axhline(y = values[3], color = "black", linestyle = "--")
+    plt.ylabel("Mean E(tw)")
+    plt.xlabel("Experiments")
+    plt.legend(["Tw", "Mean", "P10", "P90"])
+    plt.show()
+    if fuori <= 0.1 * p:
+        print "Validated!"
+        return True
+    else:
+        print "Not validated..."
+        return False
+
+values = stabilize(5, 1000, 100)
+repeated_measures(5, 1000, valori, 100)
+#simulator = ConadSimulator(5, 1000)
+#simulator.start()
+#simulator.statistics()
---
+++ README.md
@@ -0,0 +1,21 @@
+#ConadSimulator
+##A Discrete-Event Simulator of a CONAD supermarket
+
+###Overview
+This is made for a university project. Our aim is to create a discrete-event simulator to model a CONAD supermarket and analyse some variables of interest (like the mean waiting time). Below there is the full text of the assigment (in italian).
+
+Il gestore del vostro supermercato (CONAD) ha problemi per determinare il numero di cassiere da avere in uno dei periodi di maggiore affluenza.
+Poiché tua madre di lagna spesso delle attese alla cassa e ti ha sfidato a mettere in pratica la tua preparazione sulla teoria delle code, tu ti incarichi del compito di aiutare il gestore.
+Decidi quindi – a causa della complessità di decisione e di diniego, dell’abilità di aggiungere o togliere cassiere  (che incrementano o decrementano il μ di canale) e altre complessità – di simulare il modello di sistema.
+Sviluppa il modello di simulazione e implementalo nel linguaggio che tu desideri.
+Tenta di stimare, a partire da tue reali osservazioni su alcuni sabati mattina, i modelli di arrivo e di servizio, così come la disciplina delle code.
+Usa parte delle osservazioni per sviluppare le distribuzioni empiriche e convalida poi il simulatore usando le rimanenti osservazioni (al 90% del livello di confidenza). 
+Quindi cerca di determinare le soluzioni per i problemi del gestore.
+
+We used Python 2.7.10 and the SimPy library. Methods to find a steady-state and to statistically validate the simulator are provided. All statistical data had been taken from the real environment and validated using the Goodness of Fit method. NumPy and MatPlotLib libraries for Python are also required.
+
+###How it works
+The simulator is represented by the **ConadSimulator** class. The SimPy library provides a simulation environment usable via the **simpy.Environment()** method, that can handle events and their enqueuing or limited resources contended by the users of the system. We represented every single cash register as a limited resource of capacity 1 (calling **simpy.Resource()**). There is an istance of the method **entrance()** (run over the Environment using the **Environment.process()** method) that randomly generates new customers enqueuing the cash registers according to the negative exponential distribution observed and validated during the data acquisition phase and runs them over the Environment (again with the **Environment.process()** method). Every customer is represented as an istance of the **customer()** method, that simply chooses one of the cash registers and waits to use it (a process can be scheduled for a posterior time using the **yield** Python keyword). When it comes to the chosen cash register, it uses it for a time generated from a gaussian distribution (again observed and validated from the real data before), then it leaves the system. At the end of the simulation time (**Environment.run(until=duration)** lets you decide for how many time units the simulation had to go on) various statistical variables of interest are computed and printed. The **stabilize()** method compute a minimum duration time from which on the influence of the initial arbitrary state on the analysis of the simulation results becomes small, while the **repeated_measures()** one checks if the simulator is validable.
+
+###Author
+*Castellini Jacopo*
